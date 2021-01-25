8e3f580ea08224bad6898794cca930e7da465af6
cameronjchurch <cameron.j.church@gmail.com>
2021-01-24 20:22:21 -0500

    Fixed table for images

--- NewDirectionConstruction/ClientApp/src/components/Admin/Images.js
+++ NewDirectionConstruction/ClientApp/src/components/Admin/Images.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Button, Col, Form, FormGroup, Label, Input, FormText, Toast, ToastBody, ToastHeader } from 'reactstrap';
+import { Button, Col, Form, FormGroup, Label, Input, FormText, Toast, ToastBody, ToastHeader, Card, CardTitle } from 'reactstrap';
+import NdcTable from '../Common/NdcTable';
 const axios = require('axios').default;
 
 const Images = (props) => {
@@ -17,7 +18,7 @@ const Images = (props) => {
     }
 
     const deleteImage = async (e) => {
-        axios.delete('api/image/' + e.target.id).then(response => { });
+        axios.delete('api/image/' + e.target.id).then(response => { getImages(); });
     }
 
     const uploadImage = async (e) => {
@@ -32,6 +33,7 @@ const Images = (props) => {
                 "Content-Type": "multipart/form-data",
             }
         }).then(response => {
+            getImages();
             setIsUploading(false);
             setShowToast(true);
         });
@@ -49,42 +51,57 @@ const Images = (props) => {
         setShowToast(false);
     }
 
+    const columns = React.useMemo(() => [
+        { Header: 'Id', accessor: 'id' },
+        { Header: 'Title', accessor: 'title' },
+        {
+            Header: 'Image',
+            accessor: 'contents',
+            Cell: ({ cell }) => (<img src={`data:image/jpeg;base64,${cell.row.values.contents}`} />)
+        },
+        {
+            Header: 'Delete',
+            accessor: 'delete',
+            Cell: ({ cell }) => (<Button onClick={deleteImage} id={cell.row.values.id}>Delete</Button>)
+        }
+    ], []);
+
     return (
         <div>
             <h3>Images</h3>
-            {images &&
-                <div>
-                    {images.map((i) => <div>{i.id} | {i.title} | <img src={`data:image/jpeg;base64,${i.contents}`} /> | <Button onClick={deleteImage} id={i.id}>Delete</Button></div>)}
-                </div>
-            }
+            <NdcTable columns={columns} data={images} />
             <Button onClick={getImages}>Get Images</Button>
-
-            <Form onSubmit={uploadImage}>
-                <FormGroup row>
-                    <Label for="title" sm={2}>Title</Label>
-                    <Col sm={10}>
-                        <Input type="text" name="title" id="title" onChange={handleTitleChange} placeholder="Image Title" />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="file" sm={2}>File</Label>
-                    <Col sm={10}>
-                        <Input type="file" name="file" id="file" onChange={handleFileSelect} />
-                    </Col>
-                    <FormText>
-                        <section style={{ color: "white" }}>Please upload a jpg image</section>
-                    </FormText>
-                </FormGroup>
-                <FormGroup check row>
-                    <Col sm={{ size: 10, offset: 2 }}>
-                        <Button disabled={isUploading}>{isUploading ? 'Loading...' : 'Submit'}</Button>
-                    </Col>
-                </FormGroup>
-                <Toast isOpen={showToast}>
-                    <ToastHeader toggle={toggleToast}>Image Upload</ToastHeader>
-                    <ToastBody>Upload Complete</ToastBody>
-                </Toast>
-            </Form>
+            <Card outline color="secondary" style={{ margin: "17px" }}>
+                <div style={{ padding: "17px" }}>
+                    <CardTitle tag="h5">Image Upload</CardTitle>
+                    <Form onSubmit={uploadImage}>
+                        <FormGroup row>
+                            <Label for="title" sm={2}>Title</Label>
+                            <Col sm={10}>
+                                <Input type="text" name="title" id="title" onChange={handleTitleChange} placeholder="Image Title" />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="file" sm={2}>File</Label>
+                            <Col sm={10}>
+                                <Input type="file" name="file" id="file" onChange={handleFileSelect} />
+                            </Col>
+                            <FormText>
+                                <section>Please upload a jpg or png image</section>
+                            </FormText>
+                        </FormGroup>
+                        <FormGroup check row>
+                            <Col sm={{ size: 10, offset: 2 }}>
+                                <Button disabled={isUploading}>{isUploading ? 'Loading...' : 'Submit'}</Button>
+                            </Col>
+                        </FormGroup>
+                        <Toast isOpen={showToast}>
+                            <ToastHeader toggle={toggleToast}>Image Upload</ToastHeader>
+                            <ToastBody>Upload Complete</ToastBody>
+                        </Toast>
+                    </Form>
+                </div>
+            </Card>
         </div>
     );
 }
